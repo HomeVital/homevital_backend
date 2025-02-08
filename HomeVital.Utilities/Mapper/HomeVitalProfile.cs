65911ce421a17e2c776aecae7a9df4d8e2263835
Þórir Gunnar Valgeirsson <thorirv21@ru.is>
2025-02-08 20:37:02 +0000

    Fix for main deployment and migration

--- HomeVital.Utilities/Mapper/HomeVitalProfile.cs
+++ HomeVital.Utilities/Mapper/HomeVitalProfile.cs
@@ -11,8 +11,6 @@ namespace HomeVital.Utilities.Mapper
 {
     public class HomeVitalProfile : Profile
     {
-        private readonly IMapper _mapper;
-
         public HomeVitalProfile()
         {
             // Make the mapping
