ca186ef69a1deca0222b647a9e4dff22bdb7e491
LightMH <aroningi35@gmail.com>
2025-03-03 16:58:56 +0000

    ... lagatil

--- HomeVital.Repositories/Implementations/MeasurementsRepository.cs
+++ HomeVital.Repositories/Implementations/MeasurementsRepository.cs
@@ -1,58 +1,8 @@
-// using AutoMapper;
-// using HomeVital.Repositories.dbContext;
-// using HomeVital.Models.Dtos;
-// using HomeVital.Repositories.Interfaces;
-// using Microsoft.EntityFrameworkCore;
-// using HomeVital.Models.Entities;
-
-// namespace HomeVital.Repositories.Implementations
-// {
-//     public class MeasurementsRepository : IMeasurementsRepository
-//     {
-//         private readonly HomeVitalDbContext _dbContext;
-//         private readonly IMapper _mapper;
-
-//         public MeasurementsRepository(HomeVitalDbContext dbContext, IMapper mapper)
-//         {
-//             _dbContext = dbContext;
-//             _mapper = mapper;
-//         }
-
-//         public async Task<List<MeasurementDto>> GetMeasurementsById(int id)
-//         {
-//             // Query the database for measurements with the given patient id
-//             var bloodPressures = await _dbContext.BloodPressures
-//             .Where(m => m.PatientID == id)
-//             .ToListAsync();
-
-//             var bloodSugars = await _dbContext.Bloodsugars
-//             .Where(m => m.PatientID == id)
-//             .ToListAsync();
-
-//             var bodyWeights = await _dbContext.BodyWeights
-//             .Where(m => m.PatientID == id)
-//             .ToListAsync();
-
-//             var bodyTemperatures = await _dbContext.BodyTemperatures
-//             .Where(m => m.PatientID == id)
-//             .ToListAsync();
-
-//             // Map the measurements to the Dto object
-//             var measurementDto = new MeasurementDto
-        
-
-//             return new List<MeasurementDto> { measurementDto };
-//         }
-//     }
-// }
-
-
 using AutoMapper;
 using HomeVital.Repositories.dbContext;
 using HomeVital.Models.Dtos;
 using HomeVital.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using HomeVital.Models.Entities;
 
 namespace HomeVital.Repositories.Implementations
 {
